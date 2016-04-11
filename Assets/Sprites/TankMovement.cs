8fc0e55e46be9c17a231fc8dc1654179ea1b0e07
Michael <zhdelong@foxmail.com>
2016-04-11 09:24:07 +0800

    CameraSet

---
+++ Assets/Sprites/TankMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMovement : MonoBehaviour {
+
+    public float speed = 5;
+    private Rigidbody rigidbody;
+    // Use this for initialization
+	void Start () {
+        rigidbody = this.GetComponent<Rigidbody>();
+	}
+
+    void FixedUpdate() {
+        float h = Input.GetAxis("Vertical");
+        rigidbody.velocity = transform.forward * h * speed;
+    }
+}

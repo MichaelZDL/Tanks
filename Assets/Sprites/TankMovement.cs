88897f29f2f8651b24bb76acd5457ee226eb6f32
Michael <zhdelong@foxmail.com>
2016-04-11 09:53:18 +0800

    TankRotate

--- Assets/Sprites/TankMovement.cs
+++ Assets/Sprites/TankMovement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class TankMovement : MonoBehaviour {
 
     public float speed = 5;
+    public float angularSpeed = 30;
     private Rigidbody rigidbody;
     // Use this for initialization
 	void Start () {
@@ -11,7 +12,10 @@ public class TankMovement : MonoBehaviour {
 	}
 
     void FixedUpdate() {
-        float h = Input.GetAxis("Vertical");
-        rigidbody.velocity = transform.forward * h * speed;
+        float v = Input.GetAxis("Vertical");
+        rigidbody.velocity = transform.forward * v * speed;
+
+        float h = Input.GetAxis("Horizontal");
+        rigidbody.angularVelocity = transform.up * h * angularSpeed;
     }
 }

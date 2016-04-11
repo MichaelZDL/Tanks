98dd63f539e6cb0e8ba6c3d359718c327aff6404
Michael <zhdelong@foxmail.com>
2016-04-11 20:08:20 +0800

    sikiPartCompleted

--- Assets/Scripts/TankHealth.cs
+++ Assets/Scripts/TankHealth.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TankHealth : MonoBehaviour {
 
@@ -7,9 +8,12 @@ public class TankHealth : MonoBehaviour {
     public GameObject tankExplosionPrefab;
     public AudioClip tankExplosionAudio;
 
+    public Slider hpSlider;
+    private int hpTotal;
+
 	// Use this for initialization
 	void Start () {
-	
+        hpTotal = hp;
 	}
 	
 	// Update is called once per frame
@@ -20,6 +24,7 @@ public class TankHealth : MonoBehaviour {
     void TakeDamage() {
         if (hp <= 0) return;
         hp -= Random.Range(10, 20);
+        hpSlider.value = (float)hp / hpTotal;
         if (hp <= 0) {//收到伤害后死亡
             AudioSource.PlayClipAtPoint(tankExplosionAudio, transform.position);
             GameObject.Instantiate(tankExplosionPrefab, transform.position + Vector3.up, transform.rotation);
